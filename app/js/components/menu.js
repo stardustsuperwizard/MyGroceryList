1a0fc3255d2509a58d308e64d46795d5551dcd91
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2020-05-21 20:16:23 -0400

    removed border frame

--- app/js/components/menu.js
+++ app/js/components/menu.js
@@ -1,26 +1,29 @@
 const componentMenu = Vue.component('c-menu', {
     template: `
-    <div class="pure-menu">
-        <ul class="pure-menu-list">
+    <div>
+        <div id="titlebar"></div>
+        <div class="pure-menu">
+            <ul class="pure-menu-list">
 
-            <li v-bind:class="{'pure-menu-item':true, 'pure-menu-selected': ($root.currentRoute === '/options')}">
-                <router-link to="/options" class="pure-menu-link">Options</router-link>
-            </li>
+                <li v-bind:class="{'pure-menu-item':true, 'pure-menu-selected': ($root.currentRoute === '/options')}">
+                    <router-link to="/options" class="pure-menu-link">Options</router-link>
+                </li>
 
 
-            <li v-bind:class="{'pure-menu-item':true, 'pure-menu-selected': ($root.currentRoute === '/categories')}">
-                <router-link to="/categories" class="pure-menu-link">Categories</router-link>
-            </li>
+                <li v-bind:class="{'pure-menu-item':true, 'pure-menu-selected': ($root.currentRoute === '/categories')}">
+                    <router-link to="/categories" class="pure-menu-link">Categories</router-link>
+                </li>
 
-            <li v-bind:class="{'pure-menu-item':true, 'pure-menu-selected': ($root.currentRoute === '/food')}">
-                <router-link to="/food" class="pure-menu-link">Favorite Foods</router-link>
-            </li>
+                <li v-bind:class="{'pure-menu-item':true, 'pure-menu-selected': ($root.currentRoute === '/food')}">
+                    <router-link to="/food" class="pure-menu-link">Favorite Foods</router-link>
+                </li>
 
-            <li v-bind:class="{'pure-menu-item':true, 'pure-menu-selected': ($root.currentRoute === '/list')}">
-                <router-link to="/list" class="pure-menu-link">Grocery List</router-link>
-            </li>
+                <li v-bind:class="{'pure-menu-item':true, 'pure-menu-selected': ($root.currentRoute === '/list')}">
+                    <router-link to="/list" class="pure-menu-link">Grocery List</router-link>
+                </li>
 
-        </ul>
+            </ul>
+        </div>
     </div>
     `,
 });
