442a3292a2079c2932de02a6ee727c78ead987e6
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2020-07-03 17:22:05 -0400

    Update button-reset.js
    removing clear code

--- app/js/components/button-reset.js
+++ app/js/components/button-reset.js
@@ -9,14 +9,6 @@ const componentButtonReset = Vue.component('c-button-reset', {
         clear: function(e) {
             const ipc = require('electron').ipcRenderer
             localStorage.clear()
-            // let groceryListCategories = ["Bakery","Cleaning","Deli","Frozen","Grocery","Home and Office","Hygiene","Meat and Seafood","Paper Products","Pets","Pharmacy","Produce"]
-            // localStorage.setItem('categories', JSON.stringify(groceryListCategories))
-            // localStorage.setItem('filePath', filePath)
-            // console.log(JSON.stringify(data))
-            // ipc.send('clearChannel', null)
-            // ipc.on('clearChannel-reply', (event, content) => {
-            //     localStorage.setItem('filePath', content)
-            // })
             this.$emit('reset', 'None')
         }
     }
