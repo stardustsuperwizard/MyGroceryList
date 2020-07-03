c82c204a8920a48e1901425c9afcdb73ddba848d
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2020-07-03 12:36:56 -0400

    Update button-save.js
    added ;

--- app/js/components/button-save.js
+++ app/js/components/button-save.js
@@ -7,21 +7,21 @@ const componentButtonSave = Vue.component('c-button-save', {
     methods: {
         save: async function(e) {
             // console.log(e)
-            const ipc = require('electron').ipcRenderer
+            const ipc = require('electron').ipcRenderer;
             let data = {
                 filePath: localStorage.getItem('filePath')
             };
-            let tables = await idb.getTables()
+            let tables = await idb.getTables();
             for (let [key, value] of Object.entries(tables)) {
-                data[value] = await idb.readTable(value)
-            }
+                data[value] = await idb.readTable(value);
+            };
             // console.log(JSON.stringify(data))
             ipc.send('saveChannel', JSON.stringify(data))
             ipc.on('saveChannel-reply', (event, content) => {
-                localStorage.setItem('filePath', content)
-            })
-            let filePath = localStorage.getItem('filePath')
-            this.$emit('filepath', filePath)
+                localStorage.setItem('filePath', content);
+            });
+            let filePath = localStorage.getItem('filePath');
+            this.$emit('filepath', filePath);
         }
     }
 }) 
