aa909a2a3cab978b5a29ab91a30155747e953d58
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2020-07-03 13:46:39 -0400

    Update button-saveas.js
    added punctuation

--- app/js/components/button-saveas.js
+++ app/js/components/button-saveas.js
@@ -7,19 +7,18 @@ const componentButtonSaveAs = Vue.component('c-button-saveas', {
     methods: {
         save: async function(e) {
             // console.log(e)
-            const ipc = require('electron').ipcRenderer
+            const ipc = require('electron').ipcRenderer;
             let data = {};
-            let tables = await idb.getTables()
+            let tables = await idb.getTables();
             for (let [key, value] of Object.entries(tables)) {
-                data[value] = await idb.readTable(value)
-            }
+                data[value] = await idb.readTable(value);
+            };
 
-            ipc.send('saveAsChannel', JSON.stringify(data))
+            ipc.send('saveAsChannel', JSON.stringify(data));
             ipc.on('saveChannel-reply', (event, content) => {
                 localStorage.setItem('filePath', content)
             })
-            let filePath = localStorage.getItem('filePath')
-            this.$emit('filepath', filePath)
+            this.$emit('filepath', content)
         }
     }
-}) 
+});
